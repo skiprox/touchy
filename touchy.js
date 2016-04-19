4a6d33f81fc3ab58cc6519464f1e2c48d5f98a1b
Sean Scanlan <sean.scanlan@kettlenyc.com>
2016-04-19 10:23:14 -0400

    Updating with deltaX and deltaY values

--- touchy.js
+++ touchy.js
@@ -26,6 +26,8 @@ function Touchy(containerEl, obj) {
 	this._establishProperties(containerEl, obj);
 
 	// Bindings
+	this._onTouchStart = this._onTouchStart.bind(this);
+	this._onTouchEnd = this._onTouchEnd.bind(this);
 	this._onTouchMove = this._onTouchMove.bind(this);
 
 	// Add the event listeners
@@ -45,17 +47,19 @@ proto._establishProperties = function(containerEl, obj) {
 	this.settings = defaults(obj, defaultValues);
 	this.elems.container = document.querySelector(containerEl);
 	// The list of properties of the touch events we want to manually calculate and track
-	this.props.deltaStart = null;
-	this.props.deltaCurrent = null;
-	this.props.deltaEnd = null;
+	this.props.type = null;
 	this.props.deltaX = null;
 	this.props.deltaY = null;
+	this.props.deltaXStart = null;
+	this.props.deltaYStart = null;
 };
 
 /**
  * Add Event Listeners
  */
 proto._addListeners = function() {
+	this.elems.container.addEventListener('touchstart', this._onTouchStart);
+	this.elems.container.addEventListener('touchend', this._onTouchEnd);
 	this.elems.container.addEventListener('touchmove', this._onTouchMove);
 };
 
@@ -63,6 +67,8 @@ proto._addListeners = function() {
  * Remove listeners
  */
 proto._removeListeners = function() {
+	this.elems.container.removeEventListener('touchstart', this._onTouchStart);
+	this.elems.container.removeEventListener('touchend', this._onTouchEnd);
 	this.elems.container.removeEventListener('touchmove', this._onTouchMove);
 };
 
@@ -72,9 +78,34 @@ proto._removeListeners = function() {
  * So we want to attach certain things to the `event` (e), like DeltaX, DeltaY, etc.
  * Otherwise we might as well use normal touch events in our main function, instead of using Touchy.
  */
+
+// Touchstart
+proto._onTouchStart = function(e) {
+	this.props.deltaXStart = e.touches[0].clientX;
+	this.props.deltaYStart = e.touches[0].clientY;
+	this.emit('panstart', e);
+};
+
+// Touchend
+proto._onTouchEnd = function(e) {
+	this.emit('panend', e);
+};
+
 // Touchmove
 proto._onTouchMove = function(e) {
+	this.props.deltaX = e.touches[0].clientX - this.props.deltaXStart;
+	this.props.deltaY = e.touches[0].clientY - this.props.deltaYStart;
+	this.updateEventValues(e);
 	this.emit('pan', e);
+};
+
+/**
+ * Update the properties attached to e
+ * @param {Event} - e: The touch event, passed here by one of the listener functions
+ */
+proto.updateEventValues = function(e) {
+	e.deltaX = this.props.deltaX;
+	e.deltaY = this.props.deltaY;
 };
 
 /**
